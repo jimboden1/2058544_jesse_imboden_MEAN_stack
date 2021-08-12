7c1e60f7eb313f69fc289f63e71ec1b1bdb51cce
jimboden1 <jimboden1@murraystate.edu>
2021-08-11 20:50:32 -0400

    Finished project functionality

---
+++ Phase2/Project1/main.js
@@ -0,0 +1,54 @@
+try {
+    if (localStorage.getItem("JesseImbodenJSONCart") != null) {
+        var cart = JSON.parse(localStorage.getItem("JesseImbodenJSONCart"));
+    }
+    else {
+        var cart = [];
+    }
+}
+catch (error) {
+    var cart = [];
+}
+var total = 0;
+var size = 0;
+function add(i) {
+    var item = shop[i];
+    if (cart.indexOf(item) >= 0) {
+        cart[cart.indexOf(item)].quantity++;
+    }
+    else {
+        cart.push(item);
+    }
+    total += item.cost;
+    size++;
+    sessionStorage.setItem("JesseImbodenJSONCart", JSON.stringify(cart));
+}
+var item = /** @class */ (function () {
+    function item(name, cost) {
+        this.name = name;
+        this.cost = cost;
+        this.quantity = 1;
+    }
+    return item;
+}());
+var shop = [new item("pants", 40), new item("shirts", 20),
+    new item("socks", 5), new item("boots", 35),
+    new item("Shorts", 15), new item("hat", 20)];
+function displayCheckout() {
+    var result = "<h1 class=\"border-dark rounded\">Checkout</h1><div class=\"text-left\"><a onClick=\"displayShop()\">Shop</a></div>\n    <div class=\"row text-center\">\n    <div class=col-1></div><div class=col-4><h4 >Item Name</h4></div><div class=col-4><h4>Price</h4></div><h4 class=\"col-2\">Quantity</h4><div class=col-1></div>\n    <div class=col-1></div>";
+    for (var _i = 0, cart_1 = cart; _i < cart_1.length; _i++) {
+        var i = cart_1[_i];
+        result += "<h4 class=col-4>" + i.name + "</h4>\n        <h4 class=col-4>$" + i.cost + "</h4>\n        <h4 class=col-2>" + i.quantity + "</h4>\n        <div class=col-1></div><div class=col-1></div>";
+    }
+    document.getElementById("display").innerHTML = result + ("</div><h2>Total Price: $" + total + "</h2>");
+}
+function displayShop() {
+    var result = "<h1>Shop</h1>\n    <div class=\"text-left\"><a onClick=\"displayCheckout()\">Checkout</a><p></p></div>\n    <div class=\"row text-center\"><div class=\"col-2\"></div>";
+    for (var i in shop) {
+        result += "<div class=\"col-4 bg-secondary mg-5 border-dark rounded\"><h4>" + shop[i].name + "</h4><br/>\n        <h4>" + shop[i].cost + "</h4><br/>\n        <button onClick=\"add(" + i + ")\">Add</button><br/><br/></div>";
+        if ((Number(i) + 1) % 2 == 0) {
+            result += "<div class=\"col-2\"></div><div class=\"col-2\"></div>";
+        }
+    }
+    document.getElementById("display").innerHTML = result + "</div>";
+}
---
+++ Phase2/Project1/main.ts
@@ -0,0 +1,68 @@
+
+try {
+    if(localStorage.getItem("JesseImbodenJSONCart")!=null){
+        var cart:Array<item> = JSON.parse(localStorage.getItem("JesseImbodenJSONCart"));
+    }
+    else{
+        var cart:Array<item> = [];
+    }
+} 
+catch (error) {
+    var cart:Array<item> = [];
+}
+var total:number = 0;
+var size:number = 0;
+
+function add(i:number){
+    let item = shop[i];
+    if(cart.indexOf(item)>=0){
+        cart[cart.indexOf(item)].quantity++;
+    }
+    else{
+        cart.push(item);
+    }
+    total+=item.cost;
+    size++;
+    sessionStorage.setItem("JesseImbodenJSONCart",JSON.stringify(cart));
+}
+
+
+class item{
+    public quantity:number = 1;
+    constructor(public name:string, public cost:number){}
+}
+
+var shop:Array<item> = [new item("pants",40), new item("shirts", 20),
+    new item("socks", 5), new item("boots", 35),
+    new item("Shorts", 15), new item("hat", 20)];
+
+
+function displayCheckout(){
+    let result:string =`<h1 class="border-dark rounded">Checkout</h1><div class="text-left"><a onClick="displayShop()">Shop</a></div>
+    <div class="row text-center">
+    <div class=col-1></div><div class=col-4><h4 >Item Name</h4></div><div class=col-4><h4>Price</h4></div><h4 class="col-2">Quantity</h4><div class=col-1></div>
+    <div class=col-1></div>`;
+    for(let i of cart){
+        result+=`<h4 class=col-4>${i.name}</h4>
+        <h4 class=col-4>$${i.cost}</h4>
+        <h4 class=col-2>${i.quantity}</h4>
+        <div class=col-1></div><div class=col-1></div>`
+    }
+    document.getElementById("display").innerHTML=result+`</div><h2>Total Price: $${total}</h2>`;
+}
+
+
+function displayShop(){
+    let result=`<h1>Shop</h1>
+    <div class="text-left"><a onClick="displayCheckout()">Checkout</a><p></p></div>
+    <div class="row text-center"><div class="col-2"></div>`;
+    for(let i in shop){
+        result+=`<div class="col-4 bg-secondary mg-5 border-dark rounded"><h4>${shop[i].name}</h4><br/>
+        <h4>${shop[i].cost}</h4><br/>
+        <button onClick="add(${i})">Add</button><br/><br/></div>`
+        if((Number(i)+1)%2==0){
+            result+=`<div class="col-2"></div><div class="col-2"></div>`
+        }
+    }
+    document.getElementById("display").innerHTML=result+`</div>`;
+}
