562a8a295eee1dd2b6b391a055f1db65d7bcb7e7
jimboden1 <jimboden1@murraystate.edu>
2021-08-24 18:04:09 -0400

    made the logging logic into a function for reusability

--- Phase3/Project1/project1.js
+++ Phase3/Project1/project1.js
@@ -1,27 +1,30 @@
-let readline = require("readline-sync");
-let fs = require("fs");
-let name = ""
-let names = [];
-let currentDate = "";
-debugger;
-try {
-    if(fs.readFileSync("names.json")!=undefined){
-        names = JSON.parse(fs.readFileSync("names.json").toString());
-    }
-} catch (error) {
-
-}
-while(name!=="0"){
+var readline = require("readline-sync");
+var fs = require("fs");
+var name = ""
+var names = [];
+var currentDate = "";
+function logData(){
     debugger;
-    console.log("Enter a name to save or enter 0 for name to end program.");
-    name = readline.question("Enter Name: ");
-    if(name!=="0"){
-        let age = readline.questionInt("Enter Age: ");
-        let email =readline.question("Enter Email: ");
-        let date = new Date();
-        currentDate = `Logged on ${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()} at ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-        names.push({name: name, age: age,email: email, logged: currentDate});
-        fs.writeFileSync("names.json",JSON.stringify(names));
+    try {
+        if(fs.readFileSync("names.json")!=undefined){
+            names = JSON.parse(fs.readFileSync("names.json").toString());
+        }
+    } catch (error) {
+
     }
+    while(name!=="0"){
+        debugger;
+        console.log("Enter a name to save or enter 0 for name to end program.");
+        name = readline.question("Enter Name: ");
+        if(name!=="0"){
+            let age = readline.questionInt("Enter Age: ");
+            let email =readline.question("Enter Email: ");
+            let date = new Date();
+            currentDate = `Logged on ${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()} at ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+            names.push({name: name, age: age,email: email, logged: currentDate});
+            fs.writeFileSync("names.json",JSON.stringify(names));
+        }
+    }
+    debugger;
 }
-debugger;
+logData();
