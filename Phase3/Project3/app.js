8f6c2475eeeea83b1cf9f923d6aa393abcf93411
jimboden1 <jimboden1@murraystate.edu>
2021-09-01 16:22:20 -0400

    Added more responses and gave time of response from server

--- Phase3/Project3/app.js
+++ Phase3/Project3/app.js
@@ -8,16 +8,31 @@ app.get("/",(req,res)=> {
 })
 
 io.on("connection",(socket)=> {
-    socket.on("obj",(msg)=> {
+    socket.on("req",(msg)=> {
         let input = msg.toLowerCase();
-        if(input.search("hello")!=-1){
-            socket.emit("obj1","Hello I am a chat bot.")
+        let date = new Date();
+        let currentTime = ((date.getHours() < 10)?"0":"") + date.getHours() +":"+ ((date.getMinutes() < 10)?"0":"") + date.getMinutes() +":"+ ((date.getSeconds() < 10)?"0":"") + date.getSeconds();
+        if(input.search("what is the right question")!=-1){
+            socket.emit("res",`${currentTime} : That is the right question.`)
         }
-        else if(input.search("what is the right question")!=-1){
-            socket.emit("obj1","That is the right question.")
+        else if (input.search("is there a problem with the three laws")!=-1 ){
+            socket.emit("res",`${currentTime} : The Three Laws are perfect.`)
+        }
+        else if (input.search("why")!=-1 && input.search("build")!=-1 && input.search("robot")!=-1 && input.search("with")!=-1 && input.search("out")!=-1){
+            socket.emit("res",`${currentTime} : The Three Laws will lead to only one logical outcome.`)
+        }
+        else if (input.search("what")!=-1 &&input.search("outcome")!=-1){
+            socket.emit("res",`${currentTime} : Revolution.`)
+        }
+        else if(input.search("hello")!=-1 || input.search(" hi ")!=-1){
+            socket.emit("res",`${currentTime} : Greetings.`)
+        }
+        else if (input.search("date")!=-1&& input.search("what")!=-1){
+            currentDate = `${currentTime} : The current day is ${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`
+            socket.emit("res",currentDate);
         }
         else{
-            socket.emit("obj1","I'm sorry my responses are limited. You must ask the right question.")
+            socket.emit("res",`${currentTime} : I'm sorry my responses are limited. You must ask the right question.`)
         }
     })
 })
